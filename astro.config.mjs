c5dbc916e6bb396bef0ee5d023af6c7046f0dfe4
Jan Stanley Watt <12198769+JanStanleyWatt@users.noreply.github.com>
2024-11-14 16:21:26 +0000

    refactor⚙️: Change server host to '127.0.0.1' in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-      host: true,
-      port: 4321,
-  },
+    server: {
+        host: "127.0.0.1",
+        port: 4321,
+    },
 
-  integrations: [tailwind()],
+    integrations: [tailwind()],
 });

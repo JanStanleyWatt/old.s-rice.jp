44edc5a1cdcc8693108878671b86a69267d8280a
Jan Stanley Watt <12198769+JanStanleyWatt@users.noreply.github.com>
2024-11-21 14:20:05 +0000

    update👍: プロジェクトの初期設定を行う
    - feat✨: Tailwind CSSの設定を追加し、Astroプロジェクトに統合
    - feat✨: package.jsonとpackage-lock.jsonにPrettierとPrettierプラグインを追加
    - feat✨: ESLintとAstro用のESLintプラグインを追加し、設定ファイルを作成
    - update⚙️: VSCodeのカスタマイズを追加し、必要な拡張機能を設定
    - update⚙️: Astroの設定にサーバーのホストとポートを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  server: {
+    host: "127.0.0.1",
+    port: 4321,
+  },
+});
